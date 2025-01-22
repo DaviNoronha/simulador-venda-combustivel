55d894ad22b4736d8cb1d9e813baee6891d70b3d
Davi Noronha <davinoronha.g@gmail.com>
2025-01-22 17:07:08 -0400

    adicionando rotas

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,8 @@ import {
     BrowserRouter as Router,
     Routes,
     Route,
+    Navigate,
+    Link
 } from "react-router-dom";
 
 import Index from './pages/Index';
@@ -10,7 +12,23 @@ import Venda from './pages/Venda';
 export function AppRoutes() {
     return (
         <Router>
+            <nav className="p-4 bg-gray-200">
+                {/* Links para navegação */}
+                <ul className="flex space-x-4">
+                    <li>
+                        <Link to="/index" className="text-blue-500 hover:underline">
+                            Index
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/venda" className="text-blue-500 hover:underline">
+                            Venda
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
             <Routes>
+                <Route path="/" element={<Navigate to="/index" replace />} />
                 <Route path="/index" element={<Index />} />
                 <Route path="/venda" element={<Venda />} />
             </Routes>
